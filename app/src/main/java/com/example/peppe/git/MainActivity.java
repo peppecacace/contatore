6c21497afb04b625f34f74bc008a621a263c6344
peppecacace <giuseppe.cacace10@gmail.com>
2018-03-26 15:28:26 +0200

    creato layout

--- app/src/main/java/com/example/peppe/git/MainActivity.java
+++ app/src/main/java/com/example/peppe/git/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.peppe.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //view layout
+    private TextView vMessaggio;
+    private Button vAggiungi;
+    private Button vDiminuisci;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //imposto riferimenti layout
+        vMessaggio= findViewById(R.id.textMessaggio);
+        vAggiungi= findViewById(R.id.btnAggiungi);
+        vDiminuisci=findViewById((R.id.btnDiminuisci);
     }
 }
